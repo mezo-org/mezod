ba19720aa75891cdeaf6f9778f2211a12efa8408
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-04-23 10:32:52 +0200

    Disable authorized networks for GKE cluster
    VPN-based cluster access is no longer our default choice. The recommended
    approach is publicly accessible control plane endpoint with GCP IAM.

--- infrastructure/terraform/mezo-staging/gke.tf
+++ infrastructure/terraform/mezo-staging/gke.tf
@@ -14,26 +14,7 @@ module "gke" {
   ip_range_services            = var.gke_subnet.services_ip_range_name
   remove_default_node_pool     = true
   enable_private_nodes         = true
-  master_global_access_enabled = false
   deletion_protection          = false
-
-  # Setting master_authorized_networks to an empty list does not disallow external
-  # access to the control plane public endpoint. The authorized network
-  # feature is disabled by default and the endpoint is accessible from the
-  # internet. Using a non-empty list enables the authorized network feature
-  # but also adds the specified networks to the allow-list. We want to restrict
-  # external access completely and use a VPN to access the control plane.
-  # This must be done beyond Terraform once the cluster is created.
-  # Possible ways are:
-  # - Use gcloud to update the cluster with the `--enable-master-authorized-networks`
-  #   flag. Do not add any authorized networks using `--master-authorized-networks`.
-  # - Use GCP console and set `Control plane authorized networks` to `Enabled`
-  #   in the `Networking` section of the cluster settings. Do not add any
-  #   authorized networks.
-  #
-  # For more details, consult:
-  # https://cloud.google.com/kubernetes-engine/docs/how-to/authorized-networks
-  master_authorized_networks = []
 
   node_pools = [
     {
