5d90fca01ae42c1f7fed9ad237304e83e7c0699b
Dmitry <dymitr.paremski@thesis.co>
2024-06-06 15:46:53 +0200

    Separation of layers
    Making msgServer and queryServer align with each other by wrapping
    Keeper struct. This approach is used by Cosmos SDK. This draws a line
    between the API and business layer.

--- x/dualstaking/keeper/query.go
+++ x/dualstaking/keeper/query.go
@@ -4,4 +4,14 @@ import (
 	"github.com/evmos/evmos/v12/x/dualstaking/types"
 )
 
-var _ types.QueryServer = Keeper{}
+var _ types.QueryServer = queryServer{}
+
+type queryServer struct {
+	Keeper
+}
+
+// NewQueryServerImpl returns an implementation of the QueryServer interface
+// for the provided Keeper.
+func NewQueryServerImpl(keeper Keeper) types.QueryServer {
+	return &queryServer{Keeper: keeper}
+}
