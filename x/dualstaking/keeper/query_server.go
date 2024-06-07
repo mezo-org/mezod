1ed710570039ec8f1055610a36798f448411016a
Dmitry <dymitr.paremski@thesis.co>
2024-06-07 12:49:36 +0200

    Renaming NewQueryServerImpl->NewQueryServer

--- x/dualstaking/keeper/query_server.go
+++ x/dualstaking/keeper/query_server.go
@@ -15,9 +15,9 @@ type queryServer struct {
 	Keeper
 }
 
-// NewQueryServerImpl returns an implementation of the QueryServer interface
+// NewQueryServer returns an implementation of the QueryServer interface
 // for the provided Keeper.
-func NewQueryServerImpl(keeper Keeper) types.QueryServer {
+func NewQueryServer(keeper Keeper) types.QueryServer {
 	return &queryServer{Keeper: keeper}
 }
 
