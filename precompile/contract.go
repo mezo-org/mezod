c87a5fb18b05bc3e9d54199752f3ce03a356605d
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-06-11 13:27:59 +0200

    Documentation for `contract.go` file

--- precompile/contract.go
+++ precompile/contract.go
@@ -12,12 +12,14 @@ import (
 
 var _ vm.PrecompiledContract = &Contract{}
 
+// Contract represents a precompiled contract that can be executed by the EVM.
 type Contract struct {
-	abi abi.ABI
+	abi     abi.ABI
 	address common.Address
 	methods map[string]Method
 }
 
+// NewContract creates a new precompiled contract with the given ABI and address.
 func NewContract(abi abi.ABI, address common.Address) *Contract {
 	return &Contract{
 		abi:     abi,
@@ -26,6 +28,10 @@ func NewContract(abi abi.ABI, address common.Address) *Contract {
 	}
 }
 
+// RegisterMethods registers the given methods in the contract. If a method with
+// the same name already exists, it will be overwritten. This function does not
+// check whether the registered method name exists in the ABI - if not, the
+// method will not be available for callers.
 func (c *Contract) RegisterMethods(methods ...Method) *Contract {
 	for _, method := range methods {
 		c.methods[method.MethodName()] = method
@@ -34,10 +40,15 @@ func (c *Contract) RegisterMethods(methods ...Method) *Contract {
 	return c
 }
 
+// Address returns the EVM address of the contract.
 func (c *Contract) Address() common.Address {
 	return c.address
 }
 
+// RequiredGas returns the amount of gas required to execute the contract call
+// with the given input. If the target method does not determine the required
+// gas by itself, the required gas is calculated based on the default algorithm
+// that implies a flat cost plus additional cost based on the input size.
 func (c *Contract) RequiredGas(input []byte) uint64 {
 	methodID, methodInputArgs, err := c.parseCallInput(input)
 	if err != nil {
@@ -65,6 +76,13 @@ func (c *Contract) RequiredGas(input []byte) uint64 {
 	return requiredGas
 }
 
+// Run executes a contract call with the given input. The call is executed in
+// the context of the given EVM and contract instances. The right precompiled
+// method is determined based on the call input and the contract's ABI.
+// The execution can be performed in read-only mode, which means that only
+// Read methods can be executed. The call can also transfer value but the
+// target method must be of type Write and support payable calls. Output
+// arguments of the called method are returned as a byte slice.
 func (c *Contract) Run(
 	evm *vm.EVM,
 	contract *vm.Contract,
@@ -149,6 +167,9 @@ func (c *Contract) Run(
 	return methodOutputArgs, nil
 }
 
+// parseCallInput extracts the method ID and input arguments from the given
+// input byte slice. The method ID is expected to be the first 4 bytes of the
+// input. If the input is shorter than 4 bytes, an error is returned.
 func (c *Contract) parseCallInput(input []byte) ([]byte, []byte, error) {
 	// We expect a proper method call. In any other case, prevent the bounds
 	// out of range panic and return an error.
@@ -162,6 +183,9 @@ func (c *Contract) parseCallInput(input []byte) ([]byte, []byte, error) {
 	return methodID, methodInputArgs, nil
 }
 
+// methodByID returns the precompiled method and the corresponding ABI method
+// based on the given method ID. If the method is not found in the ABI or is
+// not registered in the precompiled contract, an error is returned.
 func (c *Contract) methodByID(methodID []byte) (Method, *abi.Method, error) {
 	methodABI, err := c.abi.MethodById(methodID)
 	if err != nil {
@@ -185,12 +209,16 @@ func (c *Contract) methodByID(methodID []byte) (Method, *abi.Method, error) {
 	return method, methodABI, nil
 }
 
+// RunContext represents the context in which a precompiled contract method is
+// executed. It provides access to the EVM, the contract, and the event emitter.
 type RunContext struct {
 	evm *vm.EVM
 	contract *vm.Contract
 	eventEmitter *EventEmitter
 }
 
+// NewRunContext creates a new run context with the given EVM, contract, and
+// event emitter instances.
 func NewRunContext(
 	evm *vm.EVM,
 	contract *vm.Contract,
@@ -203,14 +231,20 @@ func NewRunContext(
 	}
 }
 
+// MsgSender returns the address of the message sender. This corresponds to the
+// msg.sender in Solidity.
 func (rc *RunContext) MsgSender() common.Address {
 	return rc.contract.Caller()
 }
 
+// TxOrigin returns the address of the transaction originator. This corresponds
+// to the tx.origin in Solidity.
 func (rc *RunContext) TxOrigin() common.Address {
 	return rc.evm.Origin
 }
 
+// MsgValue returns the value sent with the message. This corresponds to the
+// msg.value in Solidity.
 func (rc *RunContext) MsgValue() *big.Int {
 	if value := rc.contract.Value(); value != nil {
 		return value
@@ -219,10 +253,13 @@ func (rc *RunContext) MsgValue() *big.Int {
 	return big.NewInt(0)
 }
 
+// IsMsgValue returns true if the message value is greater than zero.
 func (rc *RunContext) IsMsgValue() bool {
 	return rc.MsgValue().Sign() > 0
 }
 
+// EventEmitter returns the event emitter instance associated with the run context.
+// The event emitter can be used to emit EVM events from the precompiled contract.
 func (rc *RunContext) EventEmitter() *EventEmitter {
 	return rc.eventEmitter
 }
