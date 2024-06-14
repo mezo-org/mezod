1eeb2ca263257d653aa95d8df6c502a6fe84c328
Tomasz Slabon <tomasz.slabon@keep.network>
2024-06-14 17:30:55 +0200

    Uncommented code

--- x/dualstaking/keeper/msg_server_test.go
+++ x/dualstaking/keeper/msg_server_test.go
@@ -1,16 +1,17 @@
 package keeper_test
 
-// import (
-// 	"context"
-// 	"testing"
+import (
+	"context"
+	"testing"
 
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
-// 	keepertest "github.com/evmos/evmos/v12/testutil/keeper"
-// 	"github.com/evmos/evmos/v12/x/dualstaking/keeper"
-// 	"github.com/evmos/evmos/v12/x/dualstaking/types"
-// )
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	keepertest "github.com/evmos/evmos/v12/testutil/keeper"
+	"github.com/evmos/evmos/v12/x/dualstaking/keeper"
+	"github.com/evmos/evmos/v12/x/dualstaking/types"
+)
 
-// func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
-// 	k, ctx := keepertest.DualstakingKeeper(t)
-// 	return keeper.NewMsgServer(*k), sdk.WrapSDKContext(ctx)
-// }
+//nolint:unused //remove if not used in the future
+func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
+	k, ctx := keepertest.DualstakingKeeper(t)
+	return keeper.NewMsgServer(*k), sdk.WrapSDKContext(ctx)
+}
