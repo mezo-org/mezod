8fec069f1089b83e91f48c5612afc9fa343c0418
Dmitry <dymitr.paremski@thesis.co>
2024-07-09 10:34:07 +0200

    Refactoring tests

--- precompile/btctoken/matadata_test.go
+++ precompile/btctoken/matadata_test.go
@@ -21,83 +21,43 @@ var validMetadata = banktypes.Metadata{
 	},
 }
 
-func (s *PrecompileTestSuite) TestName() {
+func (s *PrecompileTestSuite) setup() {
 	bankKeeper := s.app.BankKeeper
 	btcTokenPrecompile, err := btctoken.NewPrecompile(bankKeeper)
 	s.Require().NoError(err)
 	s.btcTokenPrecompile = btcTokenPrecompile
 
 	s.app.BankKeeper.SetDenomMetaData(s.ctx, validMetadata)
+}
 
+func (s *PrecompileTestSuite) runTest(input []byte, expected interface{}, methodName string) {
 	evm := &vm.EVM{
 		StateDB: statedb.New(s.ctx, nil, statedb.TxConfig{}),
 	}
 
 	vmContract := vm.NewContract(&precompile.Contract{}, nil, nil, 0)
-	// These first 4 bytes correspond to the method ID (first 4 bytes of the
-	// Keccak-256 hash of the function signature).
-	// In this case a function signature is 'function name()'
-	vmContract.Input = []byte{0x06, 0xfd, 0xde, 0x03}
+	vmContract.Input = input
 	output, err := s.btcTokenPrecompile.Run(evm, vmContract, true)
 	s.Require().NoError(err)
 
-	method := s.btcTokenPrecompile.Abi.Methods["name"]
+	method := s.btcTokenPrecompile.Abi.Methods[methodName]
 
 	out, err := method.Outputs.Unpack(output)
 	s.Require().NoError(err)
-	s.Require().Equal(validMetadata.Name, out[0], "expected different name")
+	s.Require().Equal(expected, out[0], "expected different result")
+}
+
+func (s *PrecompileTestSuite) TestName() {
+	s.setup()
+	s.runTest([]byte{0x06, 0xfd, 0xde, 0x03}, validMetadata.Name, "name")
 }
 
 func (s *PrecompileTestSuite) TestSymbol() {
-	bankKeeper := s.app.BankKeeper
-	btcTokenPrecompile, err := btctoken.NewPrecompile(bankKeeper)
-	s.Require().NoError(err)
-	s.btcTokenPrecompile = btcTokenPrecompile
-
-	s.app.BankKeeper.SetDenomMetaData(s.ctx, validMetadata)
-
-	evm := &vm.EVM{
-		StateDB: statedb.New(s.ctx, nil, statedb.TxConfig{}),
-	}
-
-	vmContract := vm.NewContract(&precompile.Contract{}, nil, nil, 0)
-	// These first 4 bytes correspond to the method ID (first 4 bytes of the
-	// Keccak-256 hash of the function signature).
-	// In this case a function signature is 'function symbol()'
-	vmContract.Input = []byte{0x95, 0xd8, 0x9b, 0x41}
-	output, err := s.btcTokenPrecompile.Run(evm, vmContract, true)
-	s.Require().NoError(err)
-
-	method := s.btcTokenPrecompile.Abi.Methods["symbol"]
-
-	out, err := method.Outputs.Unpack(output)
-	s.Require().NoError(err)
-	s.Require().Equal(validMetadata.Symbol, out[0], "expected different name")
+	s.setup()
+	s.runTest([]byte{0x95, 0xd8, 0x9b, 0x41}, validMetadata.Symbol, "symbol")
 }
 
 func (s *PrecompileTestSuite) TestDecimals() {
-	bankKeeper := s.app.BankKeeper
-	btcTokenPrecompile, err := btctoken.NewPrecompile(bankKeeper)
-	s.Require().NoError(err)
-	s.btcTokenPrecompile = btcTokenPrecompile
-
-	s.app.BankKeeper.SetDenomMetaData(s.ctx, validMetadata)
-
-	evm := &vm.EVM{
-		StateDB: statedb.New(s.ctx, nil, statedb.TxConfig{}),
-	}
-
-	vmContract := vm.NewContract(&precompile.Contract{}, nil, nil, 0)
-	// These first 4 bytes correspond to the method ID (first 4 bytes of the
-	// Keccak-256 hash of the function signature).
-	// In this case a function signature is 'function decimals()'
-	vmContract.Input = []byte{0x31, 0x3c, 0xe5, 0x67}
-	output, err := s.btcTokenPrecompile.Run(evm, vmContract, true)
-	s.Require().NoError(err)
-
-	method := s.btcTokenPrecompile.Abi.Methods["decimals"]
-
-	out, err := method.Outputs.Unpack(output)
-	s.Require().NoError(err)
-	s.Require().Equal(validMetadata.DenomUnits[0].Exponent, out[0], "expected different name")
+	s.setup()
+	s.runTest([]byte{0x31, 0x3c, 0xe5, 0x67}, validMetadata.DenomUnits[0].Exponent, "decimals")
 }
