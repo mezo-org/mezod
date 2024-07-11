823e059c9278b15ecf64531c1e2777dcebd3bba2
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-07-11 14:52:22 +0200

    Adjust single-node localnet script
    Here we adjust the `local_node.sh` script that is responsible to run a
    single-node localnet. To make it work, we are replacing some of the native
    Cosmos CLI commands with our own ones, tailored to work with the `x/poa`
    module:
    - We are replacing `gentx` with `genval`. The former was creating a
      create-validator transaction specific to the `x/staking` module. Our
      `genval` just generates some data required to create a PoA validator
    - We are replacing `collect-gentxs` with `collect-genvals`. The Cosmos
      command was collecting multiple `gentx` transactions and used them to
      fill the genesis.json file. Our `collect-genvals` command does the same but
      for validator data generated using the `genval` command.

--- cmd/evmosd/root.go
+++ cmd/evmosd/root.go
@@ -62,6 +62,8 @@ import (
 	"github.com/evmos/evmos/v12/app"
 	cmdcfg "github.com/evmos/evmos/v12/cmd/config"
 	evmoskr "github.com/evmos/evmos/v12/crypto/keyring"
+
+	poacli "github.com/evmos/evmos/v12/x/poa/client/cli"
 )
 
 const (
@@ -125,9 +127,9 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 		evmosclient.ValidateChainID(
 			InitCmd(app.ModuleBasics, app.DefaultNodeHome),
 		),
-		genutilcli.CollectGenTxsCmd(banktypes.GenesisBalancesIterator{}, app.DefaultNodeHome),
 		MigrateGenesisCmd(),
-		genutilcli.GenTxCmd(app.ModuleBasics, encodingConfig.TxConfig, banktypes.GenesisBalancesIterator{}, app.DefaultNodeHome),
+		poacli.NewGenValCmd(app.DefaultNodeHome),
+		poacli.NewCollectGenValsCmd(app.DefaultNodeHome),
 		genutilcli.ValidateGenesisCmd(app.ModuleBasics),
 		AddGenesisAccountCmd(app.DefaultNodeHome),
 		tmcli.NewCompletionCmd(rootCmd, true),
