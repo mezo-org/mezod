4b4b1bf1316b24dbc70f8d7903c077948ba2cf51
Dmitry <dymitr.paremski@thesis.co>
2024-07-22 16:09:28 +0200

    Moving Approval event to approve.go to improve navigation across the files

--- precompile/btctoken/approve.go
+++ precompile/btctoken/approve.go
@@ -176,3 +176,46 @@ func (am approveMethod) updateAuthorization(ctx sdk.Context, grantee, granter co
 func isZeroAddress(address common.Address) bool {
 	return address == common.Address{}
 }
+
+// ApprovalEvent is the implementation of the Approval event that contains
+// the following arguments:
+// - owner (indexed): the address of BTC owner,
+// - to (indexed): the address of spender,
+// - amount (non-indexed): the amount of tokens approved.
+type ApprovalEvent struct {
+	from, to common.Address
+	amount   *big.Int
+}
+
+// ApprovalEventName is the name of the Approval event. It matches the name
+// of the event in the contract ABI.
+const ApprovalEventName = "Approval"
+
+func NewApprovalEvent(from, to common.Address, amount *big.Int) *ApprovalEvent {
+	return &ApprovalEvent{
+		from:   from,
+		to:     to,
+		amount: amount,
+	}
+}
+
+func (ae *ApprovalEvent) EventName() string {
+	return ApprovalEventName
+}
+
+func (ae *ApprovalEvent) Arguments() []*precompile.EventArgument {
+	return []*precompile.EventArgument{
+		{
+			Indexed: true,
+			Value:   ae.from,
+		},
+		{
+			Indexed: true,
+			Value:   ae.to,
+		},
+		{
+			Indexed: false,
+			Value:   ae.amount,
+		},
+	}
+}
--- precompile/btctoken/approve_test.go
+++ precompile/btctoken/approve_test.go
@@ -238,3 +238,41 @@ func unpackGrantAuthzs(grantAuthzs []*authz.GrantAuthorization) ([]authz.Authori
 
 	return auths, nil
 }
+
+func (s *PrecompileTestSuite) TestEmitApprovalEvent() {
+	testcases := []struct {
+		name    string
+		owner   common.Address
+		spender common.Address
+		amount  *big.Int
+	}{
+		{
+			name:    "pass",
+			owner:   s.account1.EvmAddr,
+			spender: s.account2.EvmAddr,
+			amount:  big.NewInt(100),
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc
+		s.Run(tc.name, func() {
+			ae := btctoken.NewApprovalEvent(tc.owner, tc.spender, tc.amount)
+			args := ae.Arguments()
+
+			s.Require().Len(args, 3)
+
+			// Check the first argument
+			s.Require().True(args[0].Indexed)
+			s.Require().Equal(tc.owner, args[0].Value)
+
+			// Check the second argument
+			s.Require().True(args[1].Indexed)
+			s.Require().Equal(tc.spender, args[1].Value)
+
+			// Check the third argument
+			s.Require().False(args[2].Indexed)
+			s.Require().Equal(tc.amount, args[2].Value)
+		})
+	}
+}
