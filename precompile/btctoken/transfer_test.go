a5e5d4170bbb7603d0366072cebcbfccd5fa4f36
Dmitry <dymitr.paremski@thesis.co>
2024-07-29 10:08:42 +0200

    Added a test for a successful path of the transfer method.

--- precompile/btctoken/transfer_test.go
+++ precompile/btctoken/transfer_test.go
@@ -1,7 +1,6 @@
 package btctoken_test
 
 import (
-	"fmt"
 	"math/big"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -16,7 +15,6 @@ import (
 )
 
 func (s *PrecompileTestSuite) TestTransfer() {
-
 	testcases := []struct {
 		name        string
 		run         func() []interface{}
@@ -72,11 +70,33 @@ func (s *PrecompileTestSuite) TestTransfer() {
 			basicPass:   true,
 			errContains: "insufficient funds",
 		},
-		// TODO: Add test for successful transfer
+		{
+			name: "successful transfer",
+			run: func() []interface{} {
+				// Mint some coins to the module account and then send to the from address
+				err := s.app.BankKeeper.MintCoins(s.ctx, evmtypes.ModuleName, sdk.Coins{sdk.NewCoin(utils.BaseDenom, sdkmath.NewInt(1e18))})
+				s.Require().NoError(err, "failed to mint coins")
+				err = s.app.BankKeeper.SendCoinsFromModuleToAccount(s.ctx, evmtypes.ModuleName, s.account1.EvmAddr.Bytes(), sdk.Coins{sdk.NewCoin(utils.BaseDenom, sdkmath.NewInt(42))})
+				s.Require().NoError(err, "failed to send coins from module to account")
+
+				return []interface{}{
+					s.account2.EvmAddr, big.NewInt(40),
+				}
+			},
+			basicPass: true,
+			postCheck: func() {
+				// Check the balance of the two accounts
+				acc1 := s.app.BankKeeper.GetBalance(s.ctx, s.account1.SdkAddr, utils.BaseDenom)
+				acc2 := s.app.BankKeeper.GetBalance(s.ctx, s.account2.SdkAddr, utils.BaseDenom)
+				s.Require().Equal(sdkmath.NewInt(2), acc1.Amount)
+				s.Require().Equal(sdkmath.NewInt(40), acc2.Amount)
+			},
+		},
 	}
 
 	for _, tc := range testcases {
 		s.Run(tc.name, func() {
+			s.SetupTest()
 			evm := &vm.EVM{
 				StateDB: statedb.New(s.ctx, nil, statedb.TxConfig{}),
 			}
@@ -121,7 +141,6 @@ func (s *PrecompileTestSuite) TestTransfer() {
 
 			out, err := method.Outputs.Unpack(output)
 			s.Require().NoError(err)
-			fmt.Println("out[0]", out[0])
 			s.Require().Equal(true, out[0], "expected different value")
 
 			if tc.postCheck != nil {
@@ -131,6 +150,6 @@ func (s *PrecompileTestSuite) TestTransfer() {
 	}
 }
 
-// TODO: add tests for `transferFrom1`
+// TODO: add tests for `transferFrom`
 
 // TODO: add tests for the `transfer` event
