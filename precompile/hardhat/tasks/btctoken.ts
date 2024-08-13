49c1515407f72eedcaa72f1e87c7a18c9c60e268
Luke Williams <iquidus@users.noreply.github.com>
2024-08-13 11:25:07 -0400

    Set correct `precompileAddress` for `btctoken` hardhat tasks
    Fixes the `btctoken` precompile address used for hardhat tasks
    
    Changes the task name prefix to camelcase `btcToken`
    
    Removes `precompile/hardhat/tsconfig.json` - `tsconfig.json` in
    mezod root dir is used by CI

--- precompile/hardhat/tasks/btctoken.ts
+++ precompile/hardhat/tasks/btctoken.ts
@@ -2,9 +2,9 @@ import { task } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 
 import abi from '../../btctoken/abi.json'
-const precompileAddress = '0x7b7c000000000000000000000000000000000001'
+const precompileAddress = '0x7b7c000000000000000000000000000000000000'
 
-task('btctoken:name', 'Returns the token name', async (taskArguments, hre, runSuper) => {
+task('btcToken:name', 'Returns the token name', async (taskArguments, hre, runSuper) => {
   const btctoken = new hre.ethers.Contract(precompileAddress, abi, hre.ethers.provider)
   const name = await btctoken.name()
   console.log(name)
