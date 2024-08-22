551121c9eeb162a77fa0507dd0593a2d1d98fcaf
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-08-22 10:26:16 +0200

    Move `AssetsLockedEvent` proto `x/bridge/types`
    This proto is not specific to ABCI code but belongs to a broader
    domain managed by the `x/bridge` module. Here we move it to `x/bridge/types`
    to reflect that fact.

--- x/bridge/abci/types/vote_extension.pb.go
+++ x/bridge/abci/types/vote_extension.pb.go
@@ -4,11 +4,11 @@
 package types
 
 import (
-	cosmossdk_io_math "cosmossdk.io/math"
 	fmt "fmt"
 	_ "github.com/cosmos/cosmos-proto"
 	_ "github.com/cosmos/gogoproto/gogoproto"
 	proto "github.com/cosmos/gogoproto/proto"
+	types "github.com/mezo-org/mezod/x/bridge/types"
 	io "io"
 	math "math"
 	math_bits "math/bits"
@@ -27,7 +27,7 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // VoteExtension defines the vote extension structure for the Bitcoin bridge.
 type VoteExtension struct {
-	AssetsLockedEvents []AssetsLockedEvent `protobuf:"bytes,1,rep,name=assets_locked_events,json=assetsLockedEvents,proto3" json:"assets_locked_events"`
+	AssetsLockedEvents []types.AssetsLockedEvent `protobuf:"bytes,1,rep,name=assets_locked_events,json=assetsLockedEvents,proto3" json:"assets_locked_events"`
 }
 
 func (m *VoteExtension) Reset()         { *m = VoteExtension{} }
@@ -63,68 +63,15 @@ func (m *VoteExtension) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_VoteExtension proto.InternalMessageInfo
 
-func (m *VoteExtension) GetAssetsLockedEvents() []AssetsLockedEvent {
+func (m *VoteExtension) GetAssetsLockedEvents() []types.AssetsLockedEvent {
 	if m != nil {
 		return m.AssetsLockedEvents
 	}
 	return nil
 }
 
-// AssetsLockedEvent represents the event where inbound assets are locked in
-// the Bitcoin bridge.
-type AssetsLockedEvent struct {
-	// sequence is the unique identifier of the event.
-	Sequence cosmossdk_io_math.Int `protobuf:"bytes,1,opt,name=sequence,proto3,customtype=cosmossdk.io/math.Int" json:"sequence"`
-	// recipient is the account address to receive the locked assets on Mezo,
-	// in Bech32 format.
-	Recipient string `protobuf:"bytes,2,opt,name=recipient,proto3" json:"recipient,omitempty"`
-	// amount of assets locked, in 1e18 precision.
-	Amount cosmossdk_io_math.Int `protobuf:"bytes,3,opt,name=amount,proto3,customtype=cosmossdk.io/math.Int" json:"amount"`
-}
-
-func (m *AssetsLockedEvent) Reset()         { *m = AssetsLockedEvent{} }
-func (m *AssetsLockedEvent) String() string { return proto.CompactTextString(m) }
-func (*AssetsLockedEvent) ProtoMessage()    {}
-func (*AssetsLockedEvent) Descriptor() ([]byte, []int) {
-	return fileDescriptor_92f77ea72398eb8e, []int{1}
-}
-func (m *AssetsLockedEvent) XXX_Unmarshal(b []byte) error {
-	return m.Unmarshal(b)
-}
-func (m *AssetsLockedEvent) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	if deterministic {
-		return xxx_messageInfo_AssetsLockedEvent.Marshal(b, m, deterministic)
-	} else {
-		b = b[:cap(b)]
-		n, err := m.MarshalToSizedBuffer(b)
-		if err != nil {
-			return nil, err
-		}
-		return b[:n], nil
-	}
-}
-func (m *AssetsLockedEvent) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_AssetsLockedEvent.Merge(m, src)
-}
-func (m *AssetsLockedEvent) XXX_Size() int {
-	return m.Size()
-}
-func (m *AssetsLockedEvent) XXX_DiscardUnknown() {
-	xxx_messageInfo_AssetsLockedEvent.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_AssetsLockedEvent proto.InternalMessageInfo
-
-func (m *AssetsLockedEvent) GetRecipient() string {
-	if m != nil {
-		return m.Recipient
-	}
-	return ""
-}
-
 func init() {
 	proto.RegisterType((*VoteExtension)(nil), "mezo.bridge.v1.VoteExtension")
-	proto.RegisterType((*AssetsLockedEvent)(nil), "mezo.bridge.v1.AssetsLockedEvent")
 }
 
 func init() {
@@ -132,28 +79,23 @@ func init() {
 }
 
 var fileDescriptor_92f77ea72398eb8e = []byte{
-	// 322 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x91, 0xcd, 0x4a, 0x03, 0x31,
-	0x14, 0x85, 0x27, 0x56, 0x8a, 0x8d, 0x28, 0x38, 0x54, 0x18, 0x8b, 0x4e, 0x6b, 0xdd, 0x74, 0xd3,
-	0x84, 0x2a, 0x2e, 0x5c, 0x5a, 0x28, 0x22, 0xb8, 0xea, 0x42, 0xd0, 0xcd, 0x30, 0x3f, 0x97, 0x69,
-	0xac, 0x93, 0x5b, 0x27, 0xe9, 0x50, 0x7d, 0x0a, 0xdf, 0xc1, 0x97, 0xe9, 0xb2, 0x4b, 0x71, 0x51,
-	0xa4, 0x7d, 0x11, 0x99, 0x89, 0x55, 0xd4, 0x8d, 0xbb, 0x9b, 0x73, 0xbe, 0x9c, 0x84, 0x73, 0xe9,
-	0x51, 0x02, 0x4f, 0xc8, 0x83, 0x54, 0x44, 0x31, 0xf0, 0xac, 0xc3, 0x33, 0xd4, 0xe0, 0xc1, 0x44,
-	0x83, 0x54, 0x02, 0x25, 0x1b, 0xa5, 0xa8, 0xd1, 0xde, 0xce, 0x21, 0x66, 0x20, 0x96, 0x75, 0x6a,
-	0x7b, 0x21, 0xaa, 0x04, 0x95, 0x57, 0xb8, 0xdc, 0x1c, 0x0c, 0x5a, 0xab, 0xc6, 0x18, 0xa3, 0xd1,
-	0xf3, 0xc9, 0xa8, 0xcd, 0x3b, 0xba, 0x75, 0x8d, 0x1a, 0x7a, 0xab, 0x5c, 0xfb, 0x86, 0x56, 0x7d,
-	0xa5, 0x40, 0x2b, 0xef, 0x1e, 0xc3, 0x21, 0x44, 0x1e, 0x64, 0x20, 0xb5, 0x72, 0x48, 0xa3, 0xd4,
-	0xda, 0x3c, 0x3e, 0x64, 0x3f, 0x1f, 0x64, 0xe7, 0x05, 0x7b, 0x55, 0xa0, 0xbd, 0x9c, 0xec, 0xae,
-	0x4f, 0xe7, 0x75, 0xab, 0x6f, 0xfb, 0xbf, 0x0d, 0xd5, 0x7c, 0x21, 0x74, 0xe7, 0x0f, 0x6f, 0x9f,
-	0xd1, 0x0d, 0x05, 0x0f, 0x63, 0x90, 0x21, 0x38, 0xa4, 0x41, 0x5a, 0x95, 0xee, 0x41, 0x9e, 0xf0,
-	0x36, 0xaf, 0xef, 0x9a, 0xff, 0xab, 0x68, 0xc8, 0x04, 0xf2, 0xc4, 0xd7, 0x03, 0x76, 0x29, 0x75,
-	0xff, 0x0b, 0xb7, 0xf7, 0x69, 0x25, 0x85, 0x50, 0x8c, 0x04, 0x48, 0xed, 0xac, 0xe5, 0x77, 0xfb,
-	0xdf, 0x82, 0x7d, 0x4a, 0xcb, 0x7e, 0x82, 0x63, 0xa9, 0x9d, 0xd2, 0x7f, 0x62, 0x3f, 0xe1, 0xee,
-	0xc5, 0x74, 0xe1, 0x92, 0xd9, 0xc2, 0x25, 0xef, 0x0b, 0x97, 0x3c, 0x2f, 0x5d, 0x6b, 0xb6, 0x74,
-	0xad, 0xd7, 0xa5, 0x6b, 0xdd, 0xb6, 0x63, 0xa1, 0x07, 0xe3, 0x80, 0x85, 0x98, 0xf0, 0xbc, 0x86,
-	0x36, 0xa6, 0x71, 0x31, 0x44, 0x7c, 0xb2, 0x5a, 0x94, 0x1f, 0x84, 0x82, 0xeb, 0xc7, 0x11, 0xa8,
-	0xa0, 0x5c, 0x34, 0x7c, 0xf2, 0x11, 0x00, 0x00, 0xff, 0xff, 0x55, 0xe3, 0x26, 0x82, 0xc9, 0x01,
-	0x00, 0x00,
+	// 243 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0xce, 0x4d, 0xad, 0xca,
+	0xd7, 0x4f, 0x2a, 0xca, 0x4c, 0x49, 0x4f, 0xd5, 0x2f, 0x33, 0xd4, 0x2f, 0xcb, 0x2f, 0x49, 0x8d,
+	0x4f, 0xad, 0x28, 0x49, 0xcd, 0x2b, 0xce, 0xcc, 0xcf, 0xd3, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17,
+	0xe2, 0x03, 0x29, 0xd2, 0x83, 0x28, 0xd2, 0x2b, 0x33, 0x94, 0x92, 0x4c, 0xce, 0x2f, 0xce, 0xcd,
+	0x2f, 0x8e, 0x07, 0xcb, 0xea, 0x43, 0x38, 0x10, 0xa5, 0x52, 0x22, 0xe9, 0xf9, 0xe9, 0xf9, 0x10,
+	0x71, 0x10, 0x0b, 0x2a, 0x2a, 0x8d, 0x66, 0x0b, 0xd4, 0x28, 0xb0, 0xa4, 0x52, 0x16, 0x17, 0x6f,
+	0x58, 0x7e, 0x49, 0xaa, 0x2b, 0xcc, 0x52, 0xa1, 0x48, 0x2e, 0x91, 0xc4, 0xe2, 0xe2, 0xd4, 0x92,
+	0xe2, 0xf8, 0x9c, 0xfc, 0xe4, 0xec, 0xd4, 0x94, 0xf8, 0xd4, 0xb2, 0xd4, 0xbc, 0x92, 0x62, 0x09,
+	0x46, 0x05, 0x66, 0x0d, 0x6e, 0x23, 0x45, 0x3d, 0x54, 0xd7, 0xe8, 0x39, 0x82, 0xd5, 0xfa, 0x80,
+	0x95, 0xba, 0x82, 0x54, 0x3a, 0xb1, 0x9c, 0xb8, 0x27, 0xcf, 0x10, 0x24, 0x94, 0x88, 0x2e, 0x51,
+	0xec, 0xe4, 0x7e, 0xe2, 0x91, 0x1c, 0xe3, 0x85, 0x47, 0x72, 0x8c, 0x0f, 0x1e, 0xc9, 0x31, 0x4e,
+	0x78, 0x2c, 0xc7, 0x70, 0xe1, 0xb1, 0x1c, 0xc3, 0x8d, 0xc7, 0x72, 0x0c, 0x51, 0xba, 0xe9, 0x99,
+	0x25, 0x19, 0xa5, 0x49, 0x7a, 0xc9, 0xf9, 0xb9, 0xfa, 0x20, 0x0b, 0x74, 0xf3, 0x8b, 0xd2, 0xc1,
+	0x8c, 0x14, 0xfd, 0x0a, 0x98, 0xcb, 0x13, 0x93, 0x92, 0x33, 0xf5, 0x4b, 0x2a, 0x0b, 0x52, 0x8b,
+	0x93, 0xd8, 0xc0, 0x6e, 0x37, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0xf4, 0xd6, 0xe1, 0xb4, 0x40,
+	0x01, 0x00, 0x00,
 }
 
 func (m *VoteExtension) Marshal() (dAtA []byte, err error) {
@@ -193,56 +135,6 @@ func (m *VoteExtension) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
-func (m *AssetsLockedEvent) Marshal() (dAtA []byte, err error) {
-	size := m.Size()
-	dAtA = make([]byte, size)
-	n, err := m.MarshalToSizedBuffer(dAtA[:size])
-	if err != nil {
-		return nil, err
-	}
-	return dAtA[:n], nil
-}
-
-func (m *AssetsLockedEvent) MarshalTo(dAtA []byte) (int, error) {
-	size := m.Size()
-	return m.MarshalToSizedBuffer(dAtA[:size])
-}
-
-func (m *AssetsLockedEvent) MarshalToSizedBuffer(dAtA []byte) (int, error) {
-	i := len(dAtA)
-	_ = i
-	var l int
-	_ = l
-	{
-		size := m.Amount.Size()
-		i -= size
-		if _, err := m.Amount.MarshalTo(dAtA[i:]); err != nil {
-			return 0, err
-		}
-		i = encodeVarintVoteExtension(dAtA, i, uint64(size))
-	}
-	i--
-	dAtA[i] = 0x1a
-	if len(m.Recipient) > 0 {
-		i -= len(m.Recipient)
-		copy(dAtA[i:], m.Recipient)
-		i = encodeVarintVoteExtension(dAtA, i, uint64(len(m.Recipient)))
-		i--
-		dAtA[i] = 0x12
-	}
-	{
-		size := m.Sequence.Size()
-		i -= size
-		if _, err := m.Sequence.MarshalTo(dAtA[i:]); err != nil {
-			return 0, err
-		}
-		i = encodeVarintVoteExtension(dAtA, i, uint64(size))
-	}
-	i--
-	dAtA[i] = 0xa
-	return len(dAtA) - i, nil
-}
-
 func encodeVarintVoteExtension(dAtA []byte, offset int, v uint64) int {
 	offset -= sovVoteExtension(v)
 	base := offset
@@ -266,23 +158,6 @@ func (m *VoteExtension) Size() (n int) {
 			n += 1 + l + sovVoteExtension(uint64(l))
 		}
 	}
-	return n
-}
-
-func (m *AssetsLockedEvent) Size() (n int) {
-	if m == nil {
-		return 0
-	}
-	var l int
-	_ = l
-	l = m.Sequence.Size()
-	n += 1 + l + sovVoteExtension(uint64(l))
-	l = len(m.Recipient)
-	if l > 0 {
-		n += 1 + l + sovVoteExtension(uint64(l))
-	}
-	l = m.Amount.Size()
-	n += 1 + l + sovVoteExtension(uint64(l))
 	return n
 }
 
@@ -350,158 +225,8 @@ func (m *VoteExtension) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.AssetsLockedEvents = append(m.AssetsLockedEvents, AssetsLockedEvent{})
+			m.AssetsLockedEvents = append(m.AssetsLockedEvents, types.AssetsLockedEvent{})
 			if err := m.AssetsLockedEvents[len(m.AssetsLockedEvents)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		default:
-			iNdEx = preIndex
-			skippy, err := skipVoteExtension(dAtA[iNdEx:])
-			if err != nil {
-				return err
-			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
-				return ErrInvalidLengthVoteExtension
-			}
-			if (iNdEx + skippy) > l {
-				return io.ErrUnexpectedEOF
-			}
-			iNdEx += skippy
-		}
-	}
-
-	if iNdEx > l {
-		return io.ErrUnexpectedEOF
-	}
-	return nil
-}
-func (m *AssetsLockedEvent) Unmarshal(dAtA []byte) error {
-	l := len(dAtA)
-	iNdEx := 0
-	for iNdEx < l {
-		preIndex := iNdEx
-		var wire uint64
-		for shift := uint(0); ; shift += 7 {
-			if shift >= 64 {
-				return ErrIntOverflowVoteExtension
-			}
-			if iNdEx >= l {
-				return io.ErrUnexpectedEOF
-			}
-			b := dAtA[iNdEx]
-			iNdEx++
-			wire |= uint64(b&0x7F) << shift
-			if b < 0x80 {
-				break
-			}
-		}
-		fieldNum := int32(wire >> 3)
-		wireType := int(wire & 0x7)
-		if wireType == 4 {
-			return fmt.Errorf("proto: AssetsLockedEvent: wiretype end group for non-group")
-		}
-		if fieldNum <= 0 {
-			return fmt.Errorf("proto: AssetsLockedEvent: illegal tag %d (wire type %d)", fieldNum, wire)
-		}
-		switch fieldNum {
-		case 1:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Sequence", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowVoteExtension
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthVoteExtension
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthVoteExtension
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if err := m.Sequence.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
-				return err
-			}
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Recipient", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowVoteExtension
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthVoteExtension
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthVoteExtension
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Recipient = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 3:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Amount", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowVoteExtension
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthVoteExtension
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthVoteExtension
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			if err := m.Amount.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
