271a7acff31f2e3897b9f1e417f7efd4e847ecef
Luke Williams <iquidus@users.noreply.github.com>
2024-08-31 12:09:27 -0400

    Move `IValidatorPool.sol` back into `precompile/validatorpool`
    Moves the solidity interface back to the precompile dir
    
    Adds a shell script for easily copying to interfaces to the hardhat
    dir for compile
    
    Updates .gitignore to make sure these interface copies dont get
    pushed
    
    Adds util:getCode (missing from earlier commit)

---
+++ precompile/hardhat/copy-interfaces.sh
@@ -0,0 +1,4 @@
+mkdir -p ./interfaces
+cp ../validatorpool/IValidatorPool.sol ./interfaces/
+cp ../btctoken/IBTC.sol ./interfaces/
+cp -R ../btctoken/solidity ./interfaces/
---
+++ precompile/hardhat/tasks/util.ts
@@ -0,0 +1,8 @@
+import { task } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+
+task('util:getCode', "Returns the evm bytecode associated with the given address")
+  .addParam('address', "The smart contract/precompile address")
+  .setAction(async (taskArguments, hre, runSuper) => {
+    console.log(await hre.ethers.provider.getCode(taskArguments.address, "latest"));
+  })
