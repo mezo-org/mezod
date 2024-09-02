adc534d49a371ab6740e6a720a07eba0c5cd9d19
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-09-02 16:00:52 +0200

    Count votes for specific `AssetsLocked` events
    Here we add the `assetsLockedVoteCounter` component that counts votes for
    AssetsLocked events. It is responsible for determining the sequence of
    canonical `AssetsLocked` events supported by 2/3+ of the bridge validators
    and confirmed by 2/3+ of the non-bridge validators.

--- x/bridge/abci/proposal.go
+++ x/bridge/abci/proposal.go
@@ -47,12 +47,7 @@ func (ph *ProposalHandler) PrepareProposalHandler() sdk.PrepareProposalHandler {
 			bridgetypes.ValidatorPrivilege,
 		)
 
-		// The total voting power of validators with the bridge privilege
-		// (totalVP - nonBridgeValsTotalVP).
-		bridgeValsTotalVP := int64(0)
-		// The total voting power of validators without the bridge privilege
-		// (totalVP - bridgeValsTotalVP)
-		nonBridgeValsTotalVP := int64(0)
+		voteCounter := newAssetsLockedVoteCounter()
 
 		// req.LocalLastCommit.Votes is actually a list of validators in the
 		// last CometBFT validator set, with voting information regarding
@@ -62,6 +57,7 @@ func (ph *ProposalHandler) PrepareProposalHandler() sdk.PrepareProposalHandler {
 		// vote extension is empty. This loop must take this into account.
 		for _, vote := range req.LocalLastCommit.Votes {
 			valConsAddr := sdk.ConsAddress(vote.Validator.Address)
+			valVP := vote.Validator.Power
 
 			isBridgeVal := slices.ContainsFunc(
 				bridgeValsConsAddrs,
@@ -70,11 +66,7 @@ func (ph *ProposalHandler) PrepareProposalHandler() sdk.PrepareProposalHandler {
 				},
 			)
 
-			if vp := vote.Validator.Power; isBridgeVal {
-				bridgeValsTotalVP += vp
-			} else {
-				nonBridgeValsTotalVP += vp
-			}
+			voteCounter.registerVoter(valVP, isBridgeVal)
 
 			// The vote extension validators vote on and which is delivered
 			// here is an app-level composite vote extension that contains
@@ -122,9 +114,9 @@ func (ph *ProposalHandler) PrepareProposalHandler() sdk.PrepareProposalHandler {
 				continue
 			}
 
-			// TODO: Record asset locked events from this vote extension.
-			//       Add the voting power of this validator to the non-bridge
-			//       or bridge voting power counter of each asset locked event.
+			for _, event := range voteExtension.AssetsLockedEvents {
+				voteCounter.addVote(&event, valVP, isBridgeVal)
+			}
 		}
 
 		// TODO: Aggregate a common assets locked events sequence based
@@ -145,6 +137,98 @@ func (ph *ProposalHandler) ProcessProposalHandler() sdk.ProcessProposalHandler {
 	) (*cmtabci.ResponseProcessProposal, error) {
 		// TODO: Implement the ProcessProposalHandler.
 		return nil, nil
+	}
+}
+
+// assetsLockedVoteCounter is a helper structure that counts votes for
+// AssetsLocked events. It is responsible for determining the sequence of
+// canonical AssetsLocked events supported by 2/3+ of the bridge validators
+// and confirmed by 2/3+ of the non-bridge validators.
+type assetsLockedVoteCounter struct {
+	// A map of AssetsLocked vote infos indexed by their sequence number.
+	// In case validators have a different view of the world, the given
+	// sequence number may have more than one item associated with it.
+	votesInfo map[string][]*assetsLockedVoteInfo
+	// The total voting power of validators with the bridge privilege
+	// (totalVP - nonBridgeValsTotalVP).
+	bridgeValsTotalVP int64
+	// The total voting power of validators without the bridge privilege
+	// (totalVP - bridgeValsTotalVP)
+	nonBridgeValsTotalVP int64
+}
+
+// newAssetsLockedVoteCounter creates a new assetsLockedVoteCounter instance.
+func newAssetsLockedVoteCounter() *assetsLockedVoteCounter {
+	return &assetsLockedVoteCounter{
+		votesInfo: make(map[string][]*assetsLockedVoteInfo),
+		bridgeValsTotalVP: 0,
+		nonBridgeValsTotalVP: 0,
+	}
+}
+
+// registerVoter registers a validator's voting power. It must be called for
+// each validator able to vote, regardless of whether they voted or not.
+func (alvc *assetsLockedVoteCounter) registerVoter(vp int64, isBridgeVal bool) {
+	if isBridgeVal {
+		alvc.bridgeValsTotalVP += vp
+	} else {
+		alvc.nonBridgeValsTotalVP += vp
+	}
+}
+
+// addVote adds the given voting power to the sum of the bridge or non-bridge
+// voting power behind the given AssetsLocked event.
+func (alvc *assetsLockedVoteCounter) addVote(
+	event *bridgetypes.AssetsLockedEvent,
+	vp int64,
+	isBridgeVal bool,
+) {
+	sequenceKey := event.Sequence.String()
+
+	if index := slices.IndexFunc(
+		alvc.votesInfo[sequenceKey],
+		func(v *assetsLockedVoteInfo) bool {
+			// It's enough to compare the recipient and the amount.
+			// The sequence number is used as the votesInfo map key hence
+			// it is the same for all items in the given value slice.
+			return event.Recipient == v.event.Recipient &&
+				event.Amount.Equal(v.event.Amount)
+		},
+	); index != -1 {
+		alvc.votesInfo[sequenceKey][index].add(vp, isBridgeVal)
+	} else {
+		voteInfo := newAssetsLockedVoteInfo(event)
+		voteInfo.add(vp, isBridgeVal)
+		alvc.votesInfo[sequenceKey] = append(alvc.votesInfo[sequenceKey], voteInfo)
+	}
+}
+
+// assetsLockedVoteInfo is a helper structure that holds the bridge and
+// non-bridge voting power behind a single AssetsLocked event.
+type assetsLockedVoteInfo struct {
+	event           *bridgetypes.AssetsLockedEvent
+	bridgeValsVP    int64
+	nonBridgeValsVP int64
+}
+
+// newAssetsLockedVote creates a new assetsLockedVote instance.
+func newAssetsLockedVoteInfo(
+	event *bridgetypes.AssetsLockedEvent,
+) *assetsLockedVoteInfo {
+	return &assetsLockedVoteInfo{
+		event:           event,
+		bridgeValsVP:    0,
+		nonBridgeValsVP: 0,
+	}
+}
+
+// add adds the given voting power to the sum of the bridge or non-bridge
+// voting power behind this AssetsLocked event.
+func (alvi *assetsLockedVoteInfo) add(vp int64, isBridgeVal bool) {
+	if isBridgeVal {
+		alvi.bridgeValsVP += vp
+	} else {
+		alvi.nonBridgeValsVP += vp
 	}
 }
 
