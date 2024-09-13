d8cd0ee90816f0a5258760f5cbc5168a3916b390
Luke Williams <iquidus@users.noreply.github.com>
2024-09-13 17:26:25 -0400

    Initial draft of faucet logic
    Adds draft logic for making an ERC20 transfer (WIP)

--- infrastructure/terraform/mezo-staging/gcf/faucet/distribute.go
+++ infrastructure/terraform/mezo-staging/gcf/faucet/distribute.go
@@ -1,20 +1,139 @@
 package faucet
 
 import (
+	"context"
+	"crypto/ecdsa"
 	"fmt"
+	"math/big"
 	"net/http"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"golang.org/x/crypto/sha3"
 )
 
-// amount: 0.01 BTC = 10000000000000000 abtc
-// privkey: 0x....
-// btctoken address = 0x7b7c000000000000000000000000000000000000
+// TODO: move all of these values to a terrafrom conf
+const transferAmount = 10000000000000000
+const rpcURL = "http://127.0.0.1:8545"
+const secret = ""
+const token = "0x7b7c000000000000000000000000000000000000"
 
 func init() {
 	functions.HTTP("Distribute", distribute)
 }
 
-func distribute(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+func distribute(w http.ResponseWriter, r *http.Request) {
+	// Check a `to` url query is provided
+	// note: URL.Query returns a map[string][]string
+	// url: /index.html?to=1&to=2&to=3
+	// map: { to: [ "1", "2", "3" ] }
+	query := r.URL.Query()
+	to, ok := query["to"]
+	if !ok {
+		http.Error(w, "missing `to` param", http.StatusBadRequest)
+		return
+	}
+
+	// connect client to rpc endpoint
+	client, err := ethclient.Dial(rpcURL)
+	if err != nil {
+		http.Error(w, "unable to dial `RPC_URL`", http.StatusInternalServerError)
+		return
+	}
+
+	// import `from` using privkey
+	privkey, err := crypto.HexToECDSA(secret)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	publicKey := privkey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		http.Error(w, "invalid pubkey", http.StatusInternalServerError)
+		return
+	}
+	// from
+	from := crypto.PubkeyToAddress(*publicKeyECDSA)
+	fmt.Printf("from: %s\n", from.String())
+	// token address
+	tokenAddress := common.HexToAddress(token)
+	fmt.Printf("token: %s\n", tokenAddress.String())
+
+	// value (this is the transaction value, not the token transfer amount)
+	value := big.NewInt(0) // in abtc (0 BTC)
+
+	// amount (this is the amount of tokens to transfer in abtc)
+	amount := big.NewInt(transferAmount)
+	paddedAmount := common.LeftPadBytes(amount.Bytes(), 32)
+
+	transferFnSignature := []byte("transfer(address,uint256)")
+	hash := sha3.NewLegacyKeccak256()
+	hash.Write(transferFnSignature)
+	methodID := hash.Sum(nil)[:4]
+
+	for _, v := range to {
+		// Make sure we have a valid address
+		if !common.IsHexAddress(v) {
+			http.Error(w, "invalid address", http.StatusBadRequest)
+			return
+		}
+
+		// nonce
+		nonce, err := client.PendingNonceAt(context.Background(), from)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		gasPrice, err := client.SuggestGasPrice(context.Background())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		toAddress := common.HexToAddress(v)
+		fmt.Printf("to: %s\n", toAddress.String())
+		paddedAddress := common.LeftPadBytes(toAddress.Bytes(), 32)
+
+		var data []byte
+		data = append(data, methodID...)
+		data = append(data, paddedAddress...)
+		data = append(data, paddedAmount...)
+
+		gasLimit, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
+			To:   &tokenAddress,
+			Data: data,
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		tx := types.NewTransaction(nonce, tokenAddress, value, gasLimit, gasPrice, data)
+		chainID, err := client.NetworkID(context.Background())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		signedTx, err := types.SignTx(tx, types.LatestSignerForChainID(chainID), privkey)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = client.SendTransaction(context.Background(), signedTx)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Printf("tx sent: %s", signedTx.Hash().Hex())
+	}
 }
--- infrastructure/terraform/mezo-staging/gcf/faucet/go.mod
+++ infrastructure/terraform/mezo-staging/gcf/faucet/go.mod
@@ -2,16 +2,37 @@ module github.com/mezo-org/mezod/infrastructure/terraform/mezo-staging/gcf/fauce
 
 go 1.22.5
 
-require github.com/GoogleCloudPlatform/functions-framework-go v1.9.0
+require (
+	github.com/GoogleCloudPlatform/functions-framework-go v1.9.0
+	github.com/ethereum/go-ethereum v1.14.8
+	golang.org/x/crypto v0.25.0
+)
 
 require (
 	cloud.google.com/go/functions v1.16.6 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.4 // indirect
 	github.com/cloudevents/sdk-go/v2 v2.15.2 // indirect
+	github.com/deckarep/golang-set v1.8.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/holiman/uint256 v1.3.1 // indirect
 	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 )
+
+// use Evmos geth fork
+replace github.com/ethereum/go-ethereum => github.com/evmos/go-ethereum v1.10.26-evmos-rc2
