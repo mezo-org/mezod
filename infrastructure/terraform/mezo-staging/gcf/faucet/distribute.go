383a701a4d2572be92d5427e260c6cf8127e6466
Luke Williams <iquidus@users.noreply.github.com>
2024-09-20 11:18:27 -0400

    Use env args in faucet
    Adds env args to the btc faucet

--- infrastructure/terraform/mezo-staging/gcf/faucet/distribute.go
+++ infrastructure/terraform/mezo-staging/gcf/faucet/distribute.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"math/big"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
@@ -17,10 +18,10 @@ import (
 	btctoken "github.com/mezo-org/mezod/infrastructure/terraform/mezo-staging/gcf/faucet/bindings"
 )
 
-// TODO: move all of these values to a terrafrom conf
-const transferAmount = 100
-const rpcURL = "http://127.0.0.1:8545"
-const secret = ""
+// transfer amount - 0.01 BTC
+var transferAmount = big.NewInt(10000000000000000)
+
+// BTC token precompile address
 const token = "0x7b7c000000000000000000000000000000000000"
 
 func init() {
@@ -28,7 +29,11 @@ func init() {
 }
 
 func distribute(w http.ResponseWriter, r *http.Request) {
-	// Make sure we have a valid address
+	// get env args
+	rpcURL := os.Getenv("RPCURL")
+	secret := os.Getenv("SECRET")
+
+	// make sure we have a valid address
 	address := strings.TrimPrefix(r.URL.Path, "/")
 	if !common.IsHexAddress(address) {
 		http.Error(w, "invalid address", http.StatusBadRequest)
@@ -56,6 +61,7 @@ func distribute(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "invalid pubkey", http.StatusInternalServerError)
 		return
 	}
+
 	// from
 	from := crypto.PubkeyToAddress(*publicKeyECDSA)
 
@@ -91,7 +97,7 @@ func distribute(w http.ResponseWriter, r *http.Request) {
 			return types.SignTx(tx, types.LatestSignerForChainID(chainID), privkey)
 		},
 		Value: big.NewInt(0),
-	}, to, big.NewInt(transferAmount))
+	}, to, transferAmount)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
