9b98ea712158e0382693a56ffafb1da5499ad896
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-10-22 16:08:35 +0200

    Remove the `rosetta` command
    We do not plan to use it.

--- cmd/mezod/root.go
+++ cmd/mezod/root.go
@@ -70,7 +70,6 @@ import (
 	cmdcfg "github.com/mezo-org/mezod/cmd/config"
 	mezokr "github.com/mezo-org/mezod/crypto/keyring"
 
-	rosettacmd "github.com/cosmos/rosetta/cmd"
 	escli "github.com/mezo-org/mezod/ethereum/sidecar/cli"
 )
 
@@ -160,14 +159,6 @@ func NewRootCmd() (*cobra.Command, params.EncodingConfig) {
 	if err != nil {
 		panic(err)
 	}
-
-	// add rosetta
-	rootCmd.AddCommand(
-		rosettacmd.RosettaCommand(
-			encodingConfig.InterfaceRegistry,
-			encodingConfig.Codec,
-		),
-	)
 
 	return rootCmd, encodingConfig
 }
