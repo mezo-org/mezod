a1023ac89d0688b21a747aa893fd9a214ee27e81
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-11-07 17:36:12 +0100

    Remove v1 faucet

--- infrastructure/terraform/mezo-staging/load-secrets.sh
+++ infrastructure/terraform/mezo-staging/load-secrets.sh
@@ -11,12 +11,3 @@ for CERT_NAME in "${CERT_NAMES[@]}"; do
   op inject -i $CERT_DIR/$CERT_NAME.crt.tpl -o $CERT_DIR/$CERT_NAME.crt
   op inject -i $CERT_DIR/$CERT_NAME.key.tpl -o $CERT_DIR/$CERT_NAME.key
 done
-
-## Load configuration secrets.
-
-CONFIG_DIR=./configs
-CONFIG_NAMES=("faucet-config.yaml")
-
-for CONFIG_NAME in "${CONFIG_NAMES[@]}"; do
-  op inject -i $CONFIG_DIR/$CONFIG_NAME.tpl -o $CONFIG_DIR/$CONFIG_NAME
-done
