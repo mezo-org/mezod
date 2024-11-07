fde3c2c591e643d47e283c60bdb0203e307ec500
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-11-07 12:55:34 +0100

    Faucet logic and rework HTML pages to be dynamic

--- infrastructure/cloudflare/faucet/src/index.ts
+++ infrastructure/cloudflare/faucet/src/index.ts
@@ -1,40 +1,69 @@
-import { AutoRouter, error, json, cors } from "itty-router"
+import { AutoRouter, cors, html, IRequest } from "itty-router"
+import { ethers } from "ethers"
+import { indexHTML, errorHTML, successHTML } from "#/assets.ts";
 
-const baseApi = "/api"
+const cfVerifyUrl = "https://challenges.cloudflare.com/turnstile/v0/siteverify"
 
 type Env = {
-  ASSETS: any
+  MEZO_API_URL: string
+  MEZO_FAUCET_PRIVATE_KEY: string
+  TURNSTILE_SITE_KEY: string
+  TURNSTILE_SECRET_KEY: string
 }
 
 const sendBTC = async (request: Request, env: Env) => {
-  const formData = await request.formData()
-  const address = formData.get("address")
-  const turnstile = formData.get("cf-turnstile-response")
-  return json({ address, turnstile })
+  const requestForm = await request.formData()
+  const targetAddress = requestForm.get("address") as string
+
+  let cfVerifyForm = new FormData();
+  cfVerifyForm.append('secret', env.TURNSTILE_SECRET_KEY);
+  cfVerifyForm.append('response', requestForm.get("cf-turnstile-response") as string);
+  cfVerifyForm.append('remoteip', request.headers.get('CF-Connecting-IP') as string);
+
+  const cfVerifyResult = await fetch(cfVerifyUrl, {
+    body: cfVerifyForm,
+    method: 'POST',
+  });
+
+  // @ts-ignore
+  if (!(await cfVerifyResult.json()).success) {
+    return html(errorHTML("Captcha verification failed"))
+  }
+
+  if (!ethers.isAddress(targetAddress)) {
+    return html(errorHTML("Invalid target address. Try to use a proper 20-byte hexadecimal address prefixed with 0x."))
+  }
+
+  try {
+    const wallet = new ethers.Wallet(
+      env.MEZO_FAUCET_PRIVATE_KEY,
+      ethers.getDefaultProvider(env.MEZO_API_URL)
+    )
+
+    const transaction = await wallet.sendTransaction({
+      to: targetAddress,
+      value: ethers.parseEther("0.001")
+    });
+
+    return html(successHTML(transaction.hash))
+  } catch (error) {
+    return html(errorHTML(`Unexpected error: ${error}`))
+  }
 }
 
 const { preflight, corsify } = cors()
 
 const router = AutoRouter({
-  base: baseApi,
   before: [preflight],
   finally: [corsify],
 })
 
 router
   .post("/", sendBTC)
-  .all("*", () => error(404, "Not Found"))
+  .all("*", (_: IRequest, env: Env) => html(indexHTML(env.TURNSTILE_SITE_KEY)))
 
 export default {
-  async fetch(request: Request, env: Env, context: ExecutionContext) {
-    const url = new URL(request.url);
-    if (url.pathname.startsWith(baseApi)) {
-      return router.fetch(request, env);
-    }
-
-    // Passes the incoming request through to the assets binding.
-    // No asset matched this request before the worker code was invoked,
-    // so this will evaluate `not_found_handling` behavior.
-    return env.ASSETS.fetch(request);
+  async fetch(request: Request, env: Env, _: ExecutionContext) {
+    return router.fetch(request, env)
   },
 }
