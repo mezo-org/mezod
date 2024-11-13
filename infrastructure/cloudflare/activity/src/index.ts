997fde164041b26d137ad762f963f50936cb465b
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2024-11-13 10:45:48 +0100

    Add some mock activity data

--- infrastructure/cloudflare/activity/src/index.ts
+++ infrastructure/cloudflare/activity/src/index.ts
@@ -14,7 +14,29 @@ export class InternalEntrypoint extends WorkerEntrypoint {
   async get(): Promise<InternalGetResponse> {
     try {
       // TODO: Implement the logic for fetching activity data.
-      return { success: true, activity: [] }
+
+      const mockData: ActivityItem[] = [
+        {
+          address: "0x6d2C29C7E7C206dd7A0F2D47C79Ab828E6546d7C",
+          txCount: 10,
+          deployedContracts: 5,
+          deployedContractsTxCount: 100,
+        },
+        {
+          address: "0x7964f3985F1eA5E7f58b3aa44deCe2de7468d51B",
+          txCount: 1,
+          deployedContracts: 0,
+          deployedContractsTxCount: 0,
+        },
+        {
+          address: "0x9e0F855D302C633cF78F721A5c0A419D38EE949A",
+          txCount: 500,
+          deployedContracts: 20,
+          deployedContractsTxCount: 3000,
+        }
+      ]
+
+      return { success: true, activity: mockData }
     } catch (error) {
       return { success: false, errorMsg: `${error}` }
     }
