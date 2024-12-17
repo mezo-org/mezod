ef94375cf385e7fc297134c94774bbe07e42a523
Krzysztof Łuczak <krzysztof.luczak@boar.network>
2024-12-17 15:39:17 +0100

    remove helm provider from TF

--- infrastructure/terraform/mezo-staging/providers.tf
+++ infrastructure/terraform/mezo-staging/providers.tf
@@ -3,13 +3,3 @@ provider "google" {
   region  = var.region.name
   zone    = var.region.zones[0]
 }
-
-data "google_client_config" "default" {}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.gke.endpoint
-    token                  = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(module.gke.ca_certificate)
-  }
-}
