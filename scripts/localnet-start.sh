bf7a09915021765ca7290cd969fc8dbb53a40d3c
Jeremy Letang <me@jeremyletang.com>
2025-01-08 17:02:45 +0100

    Add the sidecars to localnet scripts
    when running make localnet-bin-start, the script now also launch the ethereum
    and connect sidecars. Minimal configuration is available using a .env file in
    order to specify the ethereum RPC provider for the ethereum sidecar.
    
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>

--- scripts/localnet-start.sh
+++ scripts/localnet-start.sh
@@ -2,6 +2,17 @@
 
 HOMEDIR=./.localnet
 
+# load custom configuration via the .env file
+# for now a single option is available to set the ethereum
+# RPC provider URL, e.g:
+# ETH_SIDECAR_RPC_PROVIDER=wss://my.eth.rpc/provider
+set -a; source .env; set +a
+if [ -z "${ETH_SIDECAR_RPC_PROVIDER}" ]; then
+  echo "Please set the ETH_SIDECAR_RPC_PROVIDER variable in the .env file."
+  exit 1
+fi
+
+
 if [ -z "${LOCALNET_CHAIN_ID}" ]; then
   echo "Please set the LOCALNET_CHAIN_ID environment variable."
   exit 1
@@ -40,9 +51,49 @@ NODE_HOMEDIR="$HOMEDIR/$NODE_NAME/mezod"
 
 echo "starting node $NODE_NAME with home directory $NODE_HOMEDIR"
 
+# create a port per eth sidecar per node
+ETH_SIDECAR_PORT=$((7500 + $NODE_INDEX))
+ETH_SIDECAR_ADDRESS="0.0.0.0:$ETH_SIDECAR_PORT"
+
+#start the ethereumn sidecar
+./build/mezod ethereum-sidecar \
+  --ethereum-sidecar.server.ethereum-node-address=$ETH_SIDECAR_RPC_PROVIDER \
+  --ethereum-sidecar.server.address=$ETH_SIDECAR_ADDRESS &
+ETH_SIDECAR_PID=$!
+
+# create a port per connect sidecar per node
+CONNECT_SIDECAR_PORT=$((7600 + $NODE_INDEX))
+CONNECT_SIDECAR_ADDRESS="0.0.0.0:$CONNECT_SIDECAR_PORT"
+
+# the mezo node grpc address, required for the market map call from the connect sidecar
+MEZOD_GRPC_PORT=$((9200 + $NODE_INDEX))
+MEZOD_GRPC_ADDRESS="localhost:$MEZOD_GRPC_PORT"
+
+# start the connect sidecar
+connect --port=$CONNECT_SIDECAR_PORT --market-map-endpoint=$MEZOD_GRPC_ADDRESS &
+CONNECT_SIDECAR_PID=$!
+
+# stat the mezod binary
 ./build/mezod start --home "$NODE_HOMEDIR" \
   --chain-id=$LOCALNET_CHAIN_ID \
   --json-rpc.api="eth,web3,net,debug,miner,txpool,personal" \
-  --json-rpc.enable
+  --json-rpc.enable \
+  --ethereum-sidecar.client.server-address=$ETH_SIDECAR_ADDRESS \
+  --oracle.oracle_address=$CONNECT_SIDECAR_ADDRESS \
+  --grpc.address=$MEZOD_GRPC_ADDRESS &
+
+MEZOD_PID=$!
 
 echo "node $NODE_NAME started."
+
+# catch ctrl-c
+trap cleanup SIGINT
+
+cleanup() {
+  kill $ETH_SIDECAR_PID
+  kill $CONNECT_SIDECAR_PID
+  kill $MEZOD_PID
+  exit
+}
+
+wait
