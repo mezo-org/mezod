0055b11d67bcc9feedd7f6d1f0c63d2ca9a294ef
Luke Williams <iquidus@users.noreply.github.com>
2025-01-13 09:30:28 -0500

    Update `TestCustomPrecompileGenesisAccounts` with `Upgrade` precompile
    Adds the `Upgrade` precompile to the
    `TestCustomPrecompileGenesisAccounts` test
    
    Note: The `Upgrade` precompile is now a genesis account.

--- x/evm/keeper/keeper_test.go
+++ x/evm/keeper/keeper_test.go
@@ -188,8 +188,9 @@ func (suite *KeeperTestSuite) TestGetAccountOrEmpty() {
 
 func (suite *KeeperTestSuite) TestCustomPrecompileGenesisAccounts() {
 	accounts := suite.app.EvmKeeper.CustomPrecompileGenesisAccounts()
-	suite.Require().Equal(len(accounts), 3)
+	suite.Require().Equal(len(accounts), 4)
 	suite.Require().Equal(accounts[0].Address, "0x7b7C000000000000000000000000000000000000")
 	suite.Require().Equal(accounts[1].Address, "0x7B7C000000000000000000000000000000000011")
 	suite.Require().Equal(accounts[2].Address, "0x7B7C000000000000000000000000000000000012")
+	suite.Require().Equal(accounts[3].Address, "0x7b7c000000000000000000000000000000000014")
 }
