be8b461a7d20aabc3023690843930856e76f508a
Luke Williams <iquidus@users.noreply.github.com>
2025-01-14 20:48:58 -0500

    Use `statedb.NewMockKeeper` in `Maintenance` precompile tests

--- precompile/maintenance/setup_test.go
+++ precompile/maintenance/setup_test.go
@@ -111,7 +111,7 @@ func (s *PrecompileTestSuite) RunMethodTestCases(testcases []TestCase, methodNam
 	for _, tc := range testcases {
 		s.Run(tc.name, func() {
 			evm := &vm.EVM{
-				StateDB: statedb.New(s.ctx, nil, statedb.TxConfig{}),
+				StateDB: statedb.New(s.ctx, statedb.NewMockKeeper(), statedb.TxConfig{}),
 			}
 			maintenancePrecompile, err := maintenance.NewPrecompile(s.poaKeeper, s.evmKeeper)
 			s.Require().NoError(err)
