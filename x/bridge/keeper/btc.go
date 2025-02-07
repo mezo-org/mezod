eb4855f7bf9c8ea3216faa7f63a140ddeff6c738
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2025-02-07 14:47:34 +0100

    Make x/bridge source BTC address setter public
    This will be needed by the v0.6.0 upgrade handler.

--- x/bridge/keeper/btc.go
+++ x/bridge/keeper/btc.go
@@ -1,13 +1,10 @@
 package keeper
 
 import (
-	"fmt"
-
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/mezo-org/mezod/x/bridge/types"
-	evmtypes "github.com/mezo-org/mezod/x/evm/types"
 )
 
 // GetSourceBTCToken returns the BTC token address on the source chain.
@@ -17,7 +14,8 @@ func (k Keeper) GetSourceBTCToken(ctx sdk.Context) []byte {
 	return ctx.KVStore(k.storeKey).Get(types.SourceBTCTokenKey)
 }
 
-func (k Keeper) setSourceBTCToken(ctx sdk.Context, sourceBTCToken []byte) {
+// SetSourceBTCToken sets the BTC token address on the source chain.
+func (k Keeper) SetSourceBTCToken(ctx sdk.Context, sourceBTCToken []byte) {
 	ctx.KVStore(k.storeKey).Set(types.SourceBTCTokenKey, sourceBTCToken)
 }
 
@@ -97,24 +95,4 @@ func (k Keeper) IncreaseBTCBurnt(ctx sdk.Context, amount math.Int) error {
 	store.Set(types.BTCBurntKey, bz)
 
 	return nil
-}
-
-// applyBTCStorageMigration is used for migrations purposes. Every BTCs (BTC) or ERC20 token
-// should have they amount burn and minted tracked at all time, however this was
-// introduce only in a later upgrade. This will be call as soon as the first EndBlock call
-// for the execution of the first block after the upgrade, if the keeper didn't have a
-// storage slot for this BTC specifically, and then initialize it with the current total
-// supply known by the x/bank module.
-//
-//nolint:unused
-func (k Keeper) applyBTCStorageMigration(ctx sdk.Context) {
-	supply := k.bankKeeper.GetSupply(ctx, evmtypes.DefaultEVMDenom)
-	// if the supply is != 0, likely after an upgrade
-	if !supply.IsZero() {
-		// then we upgrade the store with the current
-		// know supply
-		if err := k.IncreaseBTCMinted(ctx, supply.Amount); err != nil {
-			panic(fmt.Sprintf("unable to migrate storage on the 1st block of an upgrade: %v", err))
-		}
-	}
 }
