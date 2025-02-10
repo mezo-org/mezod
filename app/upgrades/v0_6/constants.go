6ff66665ceb0ae2b12bdc9092793c3fcedd3e3f2
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2025-02-10 12:58:26 +0100

    Delay the v0.6.0 fork block

--- app/upgrades/v0_6/constants.go
+++ app/upgrades/v0_6/constants.go
@@ -10,9 +10,9 @@ const (
 	// UpgradeName defines the name of the upgrade.
 	UpgradeName = "v0.6.0"
 	// TestnetUpgradeHeight defines the block height at which the upgrade is
-	// triggered on testnet. This is Feb 13th 2025, around 12:00 PM UTC.
-	// https://explorer.test.mezo.org/block/countdown/2447000
-	TestnetUpgradeHeight = 2_447_000
+	// triggered on testnet. This is Feb 18th 2025, around 12:00 PM UTC.
+	// https://explorer.test.mezo.org/block/countdown/2563000
+	TestnetUpgradeHeight = 2_563_000
 )
 
 var upgradeHeight = func(chainID string) int64 {
