448a0da131656044b7b07cde3dbbeb1f6b946bdd
Jeremy Letang <me@jeremyletang.com>
2025-04-01 09:40:29 +0100

    add simple monitoring for the validators versions
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>

---
+++ cmd/monitoring/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/mezo-org/mezod/monitoring"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config", "config.json", "a path to the configuration file")
+}
+
+func main() {
+	flag.Parse()
+
+	monitoring.Start(configPath)
+}
