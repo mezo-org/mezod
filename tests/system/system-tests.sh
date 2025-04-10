083618f45d62bc56ac1bdfde03f18b7ac89f9b3d
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2025-04-10 09:40:10 +0200

    Adjust system test script to ERC20 precompile changes

--- tests/system/system-tests.sh
+++ tests/system/system-tests.sh
@@ -2,11 +2,22 @@
 # This script runs the system tests for the Mezo blockchain.
 
 # Create interfaces dir if it doesn't already exist
-mkdir -p ./contracts/interfaces
+mkdir -p ./contracts/interfaces/solidity
+
+# Copy ERC20 interfaces to the solidity sub-directory.
+cp ../../precompile/erc20/IERC20.sol ./contracts/interfaces/solidity/
+cp ../../precompile/erc20/IERC20Metadata.sol ./contracts/interfaces/solidity/
+cp ../../precompile/erc20/IERC20WithPermit.sol ./contracts/interfaces/solidity/
 
 # Copy BTC Token interfaces
 cp ../../precompile/btctoken/IBTC.sol ./contracts/interfaces
-cp -R ../../precompile/btctoken/solidity ./contracts/interfaces
+# Adjust imports in IBTC.sol
+sed -i '' 's|../erc20/|./solidity/|g' ./contracts/interfaces/IBTC.sol
+
+# Copy MEZO Token interface
+cp ../../precompile/mezotoken/IMEZO.sol ./contracts/interfaces
+# Adjust imports in IMEZO.sol
+sed -i '' 's|../erc20/|./solidity/|g' ./contracts/interfaces/IMEZO.sol
 
 # Install dependencies
 npm i
