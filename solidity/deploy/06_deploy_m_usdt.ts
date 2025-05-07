e6713e6c6215578c090f3887d3acd639c22ab1ff
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2025-05-07 15:06:11 +0200

    Deployment of mUSDT

---
+++ solidity/deploy/06_deploy_m_usdt.ts
@@ -0,0 +1,13 @@
+import type { DeployFunction } from "hardhat-deploy/types"
+import { mERC20DeployFunctionFactory } from "../helpers/deploy-helpers"
+
+const tokenContract = "mUSDT"
+const tokenName = "Mezo Tether USD"
+const tokenSymbol = "mUSDT"
+const decimals = 6
+
+const func: DeployFunction = mERC20DeployFunctionFactory(tokenContract, tokenName, tokenSymbol, decimals)
+
+export default func
+
+func.tags = [tokenContract]
