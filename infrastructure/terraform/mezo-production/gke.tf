0c56616062dc62adb8f734008916217611cd35f7
Your Name <me@jeremyletang.com>
2025-05-28 12:22:49 +0100

    add a gcp bucket via terraform to store coins logos for the explorer
    Signed-off-by: Your Name <me@jeremyletang.com>

--- infrastructure/terraform/mezo-production/gke.tf
+++ infrastructure/terraform/mezo-production/gke.tf
@@ -25,3 +25,21 @@ module "gke" {
     }
   ]
 }
+
+resource "google_storage_bucket" "explorer_logos_bucket" {
+  name     = "mezo-production-explorer-logos-bucket"
+  location = var.region.name
+
+  uniform_bucket_level_access = true
+  force_destroy               = false
+}
+
+resource "google_storage_bucket_iam_member" "public_read" {
+  bucket = google_storage_bucket.explorer_logos_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
+output "explorer_logos_bucket_url" {
+  value = "https://storage.googleapis.com/${google_storage_bucket.explorer_logos_bucket.name}"
+}
