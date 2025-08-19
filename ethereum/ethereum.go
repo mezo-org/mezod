0a9d6214225b1a48973a5af053e87236da021139
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2025-08-19 09:52:59 +0200

    Integrate bridge out bindings with Ethereum sidecar
    We just re-generated bindings for Sepolia's `MezoBridge` and temporarily made them as default for all Ethereum networks (https://github.com/mezo-org/mezod/pull/532).
    
    Now, we are integrating those bindings with the Ethereum sidecar. We are also taking the opportunity and replace all direct references to geth ABI bindings (`gen/abi`) with a custom higher-level bindings providing useful tools for transaction processing (nonce management, gas price bumps) and various sytactic sugar making code cleaner.

--- ethereum/ethereum.go
+++ ethereum/ethereum.go
@@ -2,12 +2,17 @@ package ethereum
 
 import (
 	"context"
+	"crypto/ecdsa"
 	"fmt"
 	"math/big"
 	"net/url"
 	"strings"
+	"sync"
 
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/google/uuid"
 	"github.com/ipfs/go-log"
 
 	"github.com/keep-network/keep-common/pkg/chain/ethereum"
@@ -21,9 +26,28 @@ var logger = log.Logger("mezo-ethereum")
 // provides the implementation of generic features like balance monitor,
 // block counter and similar.
 type BaseChain struct {
-	client           ethutil.EthereumClient
-	chainID          *big.Int
-	blockCounter     *ethereum.BlockCounter
+	key     *keystore.Key
+	client  ethutil.EthereumClient
+	chainID *big.Int
+
+	blockCounter *ethereum.BlockCounter
+	nonceManager *ethereum.NonceManager
+	miningWaiter *ethutil.MiningWaiter
+
+	// transactionMutex allows interested parties to forcibly serialize
+	// transaction submission.
+	//
+	// When transactions are submitted, they require a valid nonce. The nonce is
+	// equal to the count of transactions the account has submitted so far, and
+	// for a transaction to be accepted it should be monotonically greater than
+	// any previous submitted transaction. To do this, transaction submission
+	// asks the Ethereum client it is connected to for the next pending nonce,
+	// and uses that value for the transaction. Unfortunately, if multiple
+	// transactions are submitted in short order, they may all get the same
+	// nonce. Serializing submission ensures that each nonce is requested after
+	// a previous transaction has been submitted.
+	transactionMutex *sync.Mutex
+
 	finalizedBlockFn func(ctx context.Context) (*big.Int, error)
 }
 
@@ -35,6 +59,7 @@ type block struct {
 func Connect(
 	ctx context.Context,
 	config ethereum.Config,
+	privateKey *ecdsa.PrivateKey,
 ) (
 	*BaseChain,
 	error,
@@ -64,7 +89,7 @@ func Connect(
 		return nil, fmt.Errorf("ETH client does not support subscriptions")
 	}
 
-	baseChain, err := newBaseChain(ctx, config, client)
+	baseChain, err := newBaseChain(ctx, config, client, privateKey)
 	if err != nil {
 		return nil, fmt.Errorf(
 			"could not create base chain handle: [%v]",
@@ -80,7 +105,10 @@ func newBaseChain(
 	ctx context.Context,
 	config ethereum.Config,
 	client *ethclient.Client,
+	privateKey *ecdsa.PrivateKey,
 ) (*BaseChain, error) {
+	key := newKeyFromECDSA(privateKey)
+
 	chainID, err := client.ChainID(ctx)
 	if err != nil {
 		return nil, fmt.Errorf(
@@ -111,6 +139,15 @@ func newBaseChain(
 		)
 	}
 
+	nonceManager := ethutil.NewNonceManager(
+		clientWithAddons,
+		key.Address,
+	)
+
+	miningWaiter := ethutil.NewMiningWaiter(clientWithAddons, config)
+
+	transactionMutex := &sync.Mutex{}
+
 	finalizedBlockFn := func(ctx context.Context) (*big.Int, error) {
 		var finalized block
 		err = client.Client().CallContext(ctx, &finalized, "eth_getBlockByNumber", "finalized", false)
@@ -134,11 +171,43 @@ func newBaseChain(
 	}
 
 	return &BaseChain{
+		key:              key,
 		client:           clientWithAddons,
 		chainID:          chainID,
 		blockCounter:     blockCounter,
+		nonceManager:     nonceManager,
+		miningWaiter:     miningWaiter,
+		transactionMutex: transactionMutex,
 		finalizedBlockFn: finalizedBlockFn,
 	}, nil
+}
+
+func (bc *BaseChain) Key() *keystore.Key {
+	return bc.key
+}
+
+func (bc *BaseChain) Client() ethutil.EthereumClient {
+	return bc.client
+}
+
+func (bc *BaseChain) ChainID() *big.Int {
+	return bc.chainID
+}
+
+func (bc *BaseChain) BlockCounter() *ethereum.BlockCounter {
+	return bc.blockCounter
+}
+
+func (bc *BaseChain) NonceManager() *ethereum.NonceManager {
+	return bc.nonceManager
+}
+
+func (bc *BaseChain) MiningWaiter() *ethutil.MiningWaiter {
+	return bc.miningWaiter
+}
+
+func (bc *BaseChain) TransactionMutex() *sync.Mutex {
+	return bc.transactionMutex
 }
 
 func (bc *BaseChain) FinalizedBlock(ctx context.Context) (*big.Int, error) {
@@ -147,10 +216,6 @@ func (bc *BaseChain) FinalizedBlock(ctx context.Context) (*big.Int, error) {
 
 func (bc *BaseChain) WatchBlocks(ctx context.Context) <-chan uint64 {
 	return bc.blockCounter.WatchBlocks(ctx)
-}
-
-func (bc *BaseChain) Client() ethutil.EthereumClient {
-	return bc.client
 }
 
 // wrapClientAddons wraps the client instance with add-ons like logging, rate
@@ -193,5 +258,22 @@ func NetworkFromString(networkStr string) ethereum.Network {
 		return ethereum.Developer
 	default:
 		return ethereum.Unknown
+	}
+}
+
+func newKeyFromECDSA(privateKey *ecdsa.PrivateKey) *keystore.Key {
+	id, err := uuid.NewRandom()
+	if err != nil {
+		panic(fmt.Sprintf("could not create random uuid: %v", err))
+	}
+
+	if privateKey == nil {
+		panic("private key is required")
+	}
+
+	return &keystore.Key{
+		Id:         id,
+		Address:    crypto.PubkeyToAddress(privateKey.PublicKey),
+		PrivateKey: privateKey,
 	}
 }
