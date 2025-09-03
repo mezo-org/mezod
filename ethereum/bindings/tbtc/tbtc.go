5c617d52acccfcb7ce6a5043ebce24b0a047d6cf
Tomasz Slabon <tomasz.slabon@keep.network>
2025-09-03 14:20:57 +0200

    Added comment explaining how bindings can be generated manually

--- ethereum/bindings/tbtc/tbtc.go
+++ ethereum/bindings/tbtc/tbtc.go
@@ -7,9 +7,44 @@ import (
 	sepoliacontract "github.com/mezo-org/mezod/ethereum/bindings/tbtc/sepolia/gen/contract"
 )
 
-// TODO: Use sepolia bindings for now.
-//       In the future implement a mechanism allowing automatic selection of
-//       bindings based on network.
+// The tBTC bindings were generated manually with the following commands:
+//
+// 1) Make binding directories:
+//    mkdir -p ethereum/bindings/tbtc/mainnet/gen/{abi,contract,_address}
+//
+// 2) Download the package and extract it:
+//    mkdir -p tmp/contracts/@keep-network/tbtc-v2@1.8.0
+//    cd tmp/contracts/@keep-network/tbtc-v2@1.8.0
+//
+//    npm pack --silent @keep-network/tbtc-v2@1.8.0
+//    mkdir -p extracted
+//    tar -xzf keep-network-tbtc-v2-1.8.0.tgz -C extracted
+//
+//    cd ../../../../     <- Go back to the root directory
+//
+// 3) Enable module writing:
+//    go env -w GOFLAGS=-mod=mod
+//
+// 4) Generate bindings:
+//    ABI_JSON="tmp/contracts/@keep-network/tbtc-v2@1.8.0/extracted/package/artifacts/Bridge.json"
+//    BIND_ROOT="ethereum/bindings/tbtc/mainnet/gen"
+//
+//    jq .abi "$ABI_JSON" > "$BIND_ROOT/abi/Bridge.abi"
+//
+//    go run github.com/ethereum/go-ethereum/cmd/abigen --abi "$BIND_ROOT/abi/Bridge.abi" --pkg abi --type Bridge --out "$BIND_ROOT/abi/Bridge.go"
+//
+//    go run github.com/keep-network/keep-common/tools/generators/ethereum "$BIND_ROOT/abi/Bridge.abi" "$BIND_ROOT/contract/Bridge.go"
+//
+//    cp "$ABI_JSON" "$BIND_ROOT/abi/Bridge.json"
+//
+// 5) Extract contract address:
+//    jq -jr .address "$ABI_JSON" > "$BIND_ROOT/_address/Bridge"
+//
+// 6) Create the `ethereum/bindings/tbtc/mainnet/gen/gen.go` file.
+//
+// 7) Restore Go flags:
+//    go env -u GOFLAGS
+//    go mod tidy
 
 func BridgeAddress(network ethconfig.Network) string {
 	switch network {
