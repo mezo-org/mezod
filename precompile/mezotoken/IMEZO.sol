512c4f3cdd743f7b994915abe82a2b25d89fbc1d
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2025-11-19 09:15:13 +0100

    Improve docstrings

--- precompile/mezotoken/IMEZO.sol
+++ precompile/mezotoken/IMEZO.sol
@@ -7,25 +7,25 @@ import "../erc20/IERC20WithPermit.sol";
 /// @title  IMEZO
 /// @notice Interface for the MEZO token.
 interface IMEZO is IERC20WithPermit {
-    /// @notice Emitted when a new minter is set
-    /// @param minter The address of the new minter
+    /// @notice Emitted when a new minter is set.
+    /// @param minter The address of the new minter.
     event MinterSet(address indexed minter);
 
-    /// @notice Emitted when tokens are minted
-    /// @param to The address receiving the minted tokens
-    /// @param amount The amount of tokens minted
+    /// @notice Emitted when tokens are minted.
+    /// @param to The address receiving the minted tokens.
+    /// @param amount The amount of tokens minted.
     event Minted(address indexed to, uint256 amount);
 
-    /// @notice Sets the minter address (only callable by POA owner)
-    /// @param minter The address of the new minter
+    /// @notice Sets the minter address (only callable by POA owner).
+    /// @param minter The address of the new minter.
     function setMinter(address minter) external returns (bool);
 
-    /// @notice Gets the current minter address
-    /// @return The address of the current minter
+    /// @notice Gets the current minter address.
+    /// @return The address of the current minter.
     function getMinter() external view returns (address);
 
-    /// @notice Mints tokens to a recipient (only callable by minter)
-    /// @param to The address to mint tokens to
-    /// @param amount The amount of tokens to mint
+    /// @notice Mints tokens to a recipient (only callable by minter).
+    /// @param to The address to mint tokens to.
+    /// @param amount The amount of tokens to mint.
     function mint(address to, uint256 amount) external returns (bool);
 }
