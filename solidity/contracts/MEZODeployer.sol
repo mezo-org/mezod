6000b7f0ae423ed5c93163faea141f9d05050d93
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2025-12-17 14:53:46 +0100

    Non-upgradeable cross-chain MEZO
    We need to remove upgradeability
    from the MEZO token. To maintain
    flexibility, we add a possibility to
    set multiple minters and burners.
    Moreover, we add support for
    EIP712 which may be needed to
    implement gas stations for MEZO.

--- solidity/contracts/MEZODeployer.sol
+++ solidity/contracts/MEZODeployer.sol
@@ -2,8 +2,6 @@
 
 pragma solidity 0.8.29;
 
-import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
-
 import "./MEZO.sol";
 
 /// @title Cross-chain MEZO Deployer
@@ -15,8 +13,7 @@ contract MEZODeployer {
         keccak256("Bank on yourself. Bring everyday finance to your Bitcoin.");
 
     /// @notice The governance address receiving the control over the token;
-    /// @dev This is the same multisig as the one used to control Mezo contracts
-    ///      upgradeability and some protocol parameters of the chain.
+    /// @dev This is the same multisig as the one used to control protocol parameters of the chain.
     address public constant GOVERNANCE = 0x98D8899c3030741925BE630C710A98B57F397C7a;    
 
     /// @notice The address of the deployed MEZO token contract.
@@ -27,43 +24,23 @@ contract MEZODeployer {
 
     error TokenAlreadyDeployed();
 
-    /// @notice Deploys the MEZO token to the chain via create2, initializes it,
-    ///         and initiates the 2-step ownership transfer process to the governance.
+    /// @notice Deploys the MEZO token to the chain via create2, and initiates 
+    ///         the 2-step ownership transfer process to the governance.
     /// @dev IMPORTANT NOTES: 
     ///      - This method can be called by anyone
     ///      - The initial owner of the deployed token is this deployer contract
-    ///      - The initial minter of the deployed token is the governance address
+    ///      - The initial minters and burners are unset
     ///      - The governance must finalize the 2-step ownership transfer process 
     ///        by issuing an acceptance transaction
-    ///      - The governance must set the minter according to the needs
+    ///      - The governance must set the minters and burners according to the needs
     function deployToken() external {
         if (token != address(0)) {
             revert TokenAlreadyDeployed();
         }
 
-        // Deploy the implementation contract using CREATE2 and prepare the initialization data.
-        // Desipite the fact that implementations may change over time, CREATE2 is used here for 
-        // consistency to ensure the same initial implementation address for all chains.
+        // Deploy the MEZO contract using CREATE2.
         // Note that {salt: SALT} is syntactic sugar assembling a CREATE2 call under the hood.
-        MEZO impl = new MEZO{salt: SALT}();
-        bytes memory initData = abi.encodeWithSelector(
-            impl.initialize.selector,
-            "MEZO",
-            "MEZO",
-            18,
-            GOVERNANCE // Set governance as initial minter.
-        );
-
-        // Deploy the transparent proxy contract using CREATE2. Using CREATE2 here is crucial
-        // as the proxy address is the primary identifier for the token and won't change over time.
-        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy{salt: SALT}(
-            address(impl),
-            GOVERNANCE, // Set governance as ProxyAdmin owner.
-            initData
-        );
-
-        // Store the address of the deployed proxy contract.
-        token = address(proxy);
+        token = address(new MEZO{salt: SALT}());
 
         emit TokenDeployed(token);
         
