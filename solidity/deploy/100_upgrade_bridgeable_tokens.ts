bf20016a385cd9368c72db038cccb75c1534c8c0
Tomasz Slabon <tomasz.slabon@keep.network>
2026-02-12 10:04:36 +0100

    Added redeployment script for mERC20 tokens

---
+++ solidity/deploy/100_upgrade_bridgeable_tokens.ts
@@ -0,0 +1,68 @@
+import type { HardhatRuntimeEnvironment } from "hardhat/types"
+import type { DeployFunction } from "hardhat-deploy/types"
+
+const tokens = [
+  "mSolvBTC",
+  "mT",
+  "mxSolvBTC",
+  "mUSDC",
+  "mUSDT",
+  "mUSDe",
+  "mFBTC",
+  "mcbBTC",
+  "mDAI",
+  "mswBTC",
+]
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { helpers, deployments } = hre
+
+  const { deployer } = await helpers.signers.getNamedSigners()
+
+  for (const token of tokens) {
+    const existingDeployment = await deployments.getOrNull(token)
+    const isValidDeployment = existingDeployment &&
+      helpers.address.isValid(existingDeployment.address)
+
+    if (!isValidDeployment) {
+      deployments.log(`Skipping ${token} - no valid deployment found`)
+      continue
+    }
+
+    const factory = await hre.ethers.getContractFactory(
+      token,
+      { signer: deployer },
+    )
+
+    const callData = factory.interface.encodeFunctionData("initializeV2", [])
+
+    const { newImplementationAddress, preparedTransaction } =
+      await helpers.upgrades.prepareProxyUpgrade(token, token, {
+        contractName: `contracts/${token}.sol:${token}`,
+        factoryOpts: { signer: deployer },
+        callData,
+      })
+
+    if (hre.network.name !== "mainnet") {
+      deployments.log("Sending transaction to upgrade implementation...")
+      await deployer.sendTransaction(preparedTransaction)
+    }
+
+    if (hre.network.tags.verify) {
+      // We use `verify` instead of `verify:verify` as the `verify` task is defined
+      // in "@openzeppelin/hardhat-upgrades" to verify the proxy’s implementation
+      // contract, the proxy itself and any proxy-related contracts, as well as
+      // link the proxy to the implementation contract’s ABI on (Ether)scan.
+      await hre.run("verify", {
+        address: newImplementationAddress,
+      })
+    }
+  }
+}
+
+export default func
+
+func.tags = ["UpgradeBridgeableTokensPermit"]
+
+// Comment this line when running an upgrade.
+func.skip = async () => true
--- solidity/test/upgrades/mERC20.upgrades.test.ts
+++ solidity/test/upgrades/mERC20.upgrades.test.ts
@@ -5,16 +5,16 @@ const describeFn =
 
 describeFn("mERC20 tokens - upgrade tests", () => {
   const tokens = [
+    { contractName: "mSolvBTC", proxyAddress: "0xa10aD2570ea7b93d19fDae6Bd7189fF4929Bc747" },
+    { contractName: "mT", proxyAddress: "0xaaC423eDC4E3ee9ef81517e8093d52737165b71F" },
+    { contractName: "mxSolvBTC", proxyAddress: "0xdF708431162Ba247dDaE362D2c919e0fbAfcf9DE" },
+    { contractName: "mUSDC", proxyAddress: "0x04671C72Aab5AC02A03c1098314b1BB6B560c197" },
+    { contractName: "mUSDT", proxyAddress: "0xeB5a5d39dE4Ea42C2Aa6A57EcA2894376683bB8E" },
+    { contractName: "mUSDe", proxyAddress: "0xdf6542260a9F768f07030E4895083F804241F4C4" },
+    { contractName: "mFBTC", proxyAddress: "0x812fcC0Bb8C207Fd8D6165a7a1173037F43B2dB8" },
     { contractName: "mcbBTC", proxyAddress: "0x6a7CD8E1384d49f502b4A4CE9aC9eb320835c5d7" },
     { contractName: "mDAI", proxyAddress: "0x1531b6e3d51BF80f634957dF81A990B92dA4b154" },
-    { contractName: "mFBTC", proxyAddress: "0x812fcC0Bb8C207Fd8D6165a7a1173037F43B2dB8" },
-    { contractName: "mSolvBTC", proxyAddress: "0xa10aD2570ea7b93d19fDae6Bd7189fF4929Bc747" },
     { contractName: "mswBTC", proxyAddress: "0x29fA8F46CBB9562b87773c8f50a7F9F27178261c" },
-    { contractName: "mT", proxyAddress: "0xaaC423eDC4E3ee9ef81517e8093d52737165b71F" },
-    { contractName: "mUSDC", proxyAddress: "0x04671C72Aab5AC02A03c1098314b1BB6B560c197" },
-    { contractName: "mUSDe", proxyAddress: "0xdf6542260a9F768f07030E4895083F804241F4C4" },
-    { contractName: "mUSDT", proxyAddress: "0xeB5a5d39dE4Ea42C2Aa6A57EcA2894376683bB8E" },
-    { contractName: "mxSolvBTC", proxyAddress: "0xdF708431162Ba247dDaE362D2c919e0fbAfcf9DE" },
   ]
 
   for (const token of tokens) {
@@ -24,4 +24,3 @@ describeFn("mERC20 tokens - upgrade tests", () => {
     })
   }
 })
-
