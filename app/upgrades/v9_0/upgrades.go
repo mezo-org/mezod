57a8ccfe1d70988fde057e93c494352baea34288
Piotr Dyraga <piotr.dyraga@thesis.co>
2026-04-07 12:20:47 +0200

    Set defaults for triparty bridge in upgrade handler
    Set the per-request limit to 250 BTC and window limit to 750 BTC. Not
    setting the controller address as we don't have it confirmed yet. The
    block delay is 1 block.

--- app/upgrades/v9_0/upgrades.go
+++ app/upgrades/v9_0/upgrades.go
@@ -7,10 +7,12 @@ import (
 	"fmt"
 	"slices"
 
+	"cosmossdk.io/math"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/mezo-org/mezod/app/upgrades"
+	bridgekeeper "github.com/mezo-org/mezod/x/bridge/keeper"
 	evmkeeper "github.com/mezo-org/mezod/x/evm/keeper"
 	evmtypes "github.com/mezo-org/mezod/x/evm/types"
 )
@@ -29,13 +31,35 @@ func CreateUpgradeHandler(
 
 		sdkCtx.Logger().Info("running v9.0.0 upgrade handler")
 
-		err := updateAssetsBridgePrecompileVersion(sdkCtx, keepers.EvmKeeper)
+		err := setTripartyDefaults(sdkCtx, keepers.BridgeKeeper)
+		if err != nil {
+			return nil, fmt.Errorf("failed to set triparty defaults: %w", err)
+		}
+
+		err = updateAssetsBridgePrecompileVersion(sdkCtx, keepers.EvmKeeper)
 		if err != nil {
 			return nil, fmt.Errorf("failed to update assets bridge precompile version: %w", err)
 		}
 
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
+}
+
+func setTripartyDefaults(ctx sdk.Context, bridgeKeeper bridgekeeper.Keeper) error {
+	ctx.Logger().Info("begin setting assets bridge triparty defaults")
+
+	bridgeKeeper.SetTripartyPaused(ctx, false)
+
+	if err := bridgeKeeper.SetTripartyBlockDelay(ctx, 1); err != nil {
+		return err
+	}
+
+	bridgeKeeper.SetTripartyPerRequestLimit(ctx, math.NewIntWithDecimal(250, 18))
+	bridgeKeeper.SetTripartyWindowLimit(ctx, math.NewIntWithDecimal(750, 18))
+
+	ctx.Logger().Info("assets bridge triparty defaults set")
+
+	return nil
 }
 
 func updateAssetsBridgePrecompileVersion(ctx sdk.Context, evmKeeper *evmkeeper.Keeper) error {
