ddce24e2895f1acfbed7a492b82d7e0cbe93d51b
Tomasz Slabon <tomasz.slabon@keep.network>
2026-04-07 15:52:13 +0200

    Moved MaxTripartyCallbackDataLength const into a dedicated file

--- x/bridge/types/keys.go
+++ x/bridge/types/keys.go
@@ -10,10 +10,6 @@ const (
 
 	// StoreKey defines the primary module store key
 	StoreKey = ModuleName
-
-	// MaxTripartyCallbackDataLength is the maximum allowed length of
-	// callbackData in a triparty bridge request (10 x 32-byte ABI words).
-	MaxTripartyCallbackDataLength = 320
 )
 
 var (
